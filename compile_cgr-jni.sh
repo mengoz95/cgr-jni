3fe4c6047d7d8a97b945c4268cae89a935c9e371
simone <simone@LATITUDE>
2016-11-09 14:45:40 +0100

    added script to compile the "Priorities" branch

---
+++ compile_cgr-jni.sh
@@ -0,0 +1,3 @@
+if [ ! -d "$CGR_JNI_CLASSPATH" ]; then mkdir $CGR_JNI_CLASSPATH; fi
+
+ javac -cp $ONE_BIN:$CGR_JNI_CLASSPATH -d $CGR_JNI_CLASSPATH ./src/routing/*.java ./src/*.java ./src/cgr_jni/*.java ./src/report/*.java ./src/core/*.java
