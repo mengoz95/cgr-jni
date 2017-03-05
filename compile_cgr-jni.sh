6fab9e3702bec245a3e5949c46a101e2d42e6621
GiusTemp <giuseppe.tempestalive@hotmail.it>
2017-03-05 12:12:24 +0100

    documentazione e correzione bug

--- compile_cgr-jni.sh
+++
@@ -1,3 +0,0 @@
-if [ ! -d "$CGR_JNI_CLASSPATH" ]; then mkdir $CGR_JNI_CLASSPATH; fi
-
- javac -cp $ONE_BIN:$CGR_JNI_CLASSPATH -d $CGR_JNI_CLASSPATH ./src/routing/*.java ./src/*.java ./src/cgr_jni/*.java ./src/report/*.java ./src/core/*.java ./src/input/*.java
