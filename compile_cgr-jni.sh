289019ff6109af6e9b7c8a8460293837ceb43163
Giuseppe <giuseppe.tempestalive@hotmail.it>
2017-06-04 21:08:07 +0200

    Update compile_cgr-jni.sh

--- compile_cgr-jni.sh
+++ compile_cgr-jni.sh
@@ -1,3 +1,11 @@
-if [ ! -d "$CGR_JNI_CLASSPATH" ]; then mkdir $CGR_JNI_CLASSPATH; fi
+#!/bin/bash
+if [ ! -d "$CGR_JNI_CLASSPATH" ]; then 
+	mkdir $CGR_JNI_CLASSPATH; 
+fi
 
- javac -cp $ONE_BIN:$CGR_JNI_CLASSPATH -d $CGR_JNI_CLASSPATH ./src/routing/*.java ./src/*.java ./src/cgr_jni/*.java ./src/report/*.java ./src/core/*.java ./src/input/*.java
+SOURCES=$(find src -iname "*.java" | grep -v src/test/)
+echo "SOURCES: $SOURCES"
+
+
+#javac -cp $ONE_BIN:$CGR_JNI_CLASSPATH -d $CGR_JNI_CLASSPATH ./src/routing/*.java ./src/*.java ./src/cgr_jni/*.java ./src/report/*.java ./src/core/*.java ./src/input/*.java
+javac -cp $ONE_BIN:$CGR_JNI_CLASSPATH -d $CGR_JNI_CLASSPATH $SOURCES
